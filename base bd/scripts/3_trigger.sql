d5b2d674f91c278c18611272668513ede868d746
Gabriela Laiani de Freitas Loureiro <72285223+Froggyxz@users.noreply.github.com>
2025-09-21 00:13:01 +0000

    edit

--- base bd/scripts/3_trigger.sql
+++ base bd/scripts/3_trigger.sql
@@ -3,13 +3,13 @@ CREATE TRIGGER valida_especialidade_agendamento
 BEFORE INSERT ON Agendamento
 FOR EACH ROW
 BEGIN
-    DECLARE especialidade_medico VARCHAR(100);
-    DECLARE especialidade_exame VARCHAR(100);
+    DECLARE especialidade_medicos VARCHAR(100);
+    DECLARE especialidade_exames VARCHAR(100);
 
-    SELECT especialidade INTO especialidade_medico FROM Medico WHERE id = NEW.medico_id;
-    SELECT especialidade_requerida INTO especialidade_exame FROM Exame WHERE id = NEW.exame_id;
+    SELECT especialidade INTO especialidade_medicos FROM Medicos WHERE id = NEW.medicos_id;
+    SELECT especialidade_requerida INTO especialidade_exames FROM Exames WHERE id = NEW.exames_id;
 
-    IF especialidade_medico <> especialidade_exame THEN
+    IF especialidade_medicos <> especialidade_exames THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Especialidade do médico não é compatível com o exame agendado.';
     END IF;
 END;
