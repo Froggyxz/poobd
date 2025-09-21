633d152c2110278211b25d405e2f50561eea243b
Gabriela Laiani de Freitas Loureiro <72285223+Froggyxz@users.noreply.github.com>
2025-09-21 00:14:15 +0000

    editaaa

--- base bd/scripts/3_trigger.sql
+++ base bd/scripts/3_trigger.sql
@@ -6,8 +6,8 @@ BEGIN
     DECLARE especialidade_medicos VARCHAR(100);
     DECLARE especialidade_exames VARCHAR(100);
 
-    SELECT especialidade INTO especialidade_medicos FROM Medicos WHERE id = NEW.medicos_id;
-    SELECT especialidade_requerida INTO especialidade_exames FROM Exames WHERE id = NEW.exames_id;
+    SELECT especialidade INTO especialidade_medicos FROM Medicos WHERE id = NEW.id;
+    SELECT especialidade_requerida INTO especialidade_exames FROM Exames WHERE id = NEW.id;
 
     IF especialidade_medicos <> especialidade_exames THEN
         SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Especialidade do médico não é compatível com o exame agendado.';
