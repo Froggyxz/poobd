8ac19e57e71aad6e3829abcd9796a4b352531af1
Gabriela Laiani de Freitas Loureiro <72285223+Froggyxz@users.noreply.github.com>
2025-09-21 00:16:37 +0000

    padronizando

--- base bd/scripts/4_visao1.sql
+++ base bd/scripts/4_visao1.sql
@@ -1,7 +1,7 @@
 -- Visão 1: União de exames de Cardiologia e Pediatria
 CREATE VIEW exames_cardiopediatria AS
-SELECT * FROM Exame WHERE especialidade_requerida = 'Cardiologia'
+SELECT * FROM exame WHERE especialidade_requerida = 'Cardiologia'
 UNION
-SELECT * FROM Exame WHERE especialidade_requerida = 'Pediatria';
+SELECT * FROM exame WHERE especialidade_requerida = 'Pediatria';
 
 SELECT * FROM exames_cardiopediatria;
--- base bd/scripts/4_visao2.sql
+++ base bd/scripts/4_visao2.sql
@@ -2,10 +2,10 @@
 -- Visão 2: Agendamentos realizados, mostrando dados do paciente, exame e médico
 CREATE VIEW agendamentos_realizados AS
 SELECT a.id AS agendamento_id, p.nome AS paciente, m.nome AS medico, e.nome AS exame, a.data_agendamento, a.sala
-FROM Agendamento a
-JOIN Paciente p ON a.paciente_id = p.id
-JOIN Medico m ON a.medico_id = m.id
-JOIN Exame e ON a.exame_id = e.id
+FROM agendamento a
+JOIN paciente p ON a.paciente_id = p.id
+JOIN medico m ON a.medico_id = m.id
+JOIN exame e ON a.exame_id = e.id
 WHERE a.status = 'Realizado';
 
 SELECT * FROM agendamentos_realizados;
